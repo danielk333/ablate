c215c25ed940a36bae7645c6f0459a3412d8931d
danielk <daniel.kastinen@irf.se>
2021-10-26 08:46:35 +0200

    new docs setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,21 +4,17 @@
 # list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../ablate'))
+import warnings
+from datetime import date
+import ablate
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Meteoroid Ablation Models'
-copyright = '2019, Daniel Kastinen, Johan Kero'
+version ='.'.join(ablate.__version__.split('.')[:2])
+release = ablate.__version__
+copyright = f'[2019-{date.today().year}], Daniel Kastinen, Johan Kero'
 author = 'Daniel Kastinen, Johan Kero'
 
 
@@ -29,15 +25,18 @@ author = 'Daniel Kastinen, Johan Kero'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary',
+    'sphinx_gallery.gen_gallery',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -63,14 +62,36 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 
-#html_sidebars = {
-#        '**': [
-#                 'globaltoc.html',
-#                 'localtoc.html',
-#                 'relations.html',
-#                 'searchbox.html',
-#            ]
-#        }
+# -- Options for gallery extension ---------------------------------------
+sphinx_gallery_conf = {
+     'examples_dirs': '../../examples',   # path to your example scripts
+     'gallery_dirs': 'auto_gallery',  # path where to save gallery generated examples
+     'filename_pattern': '/*.py',
+     'ignore_pattern': r'.*__no_agl\.py',
+}
+
+# Remove matplotlib agg warnings from generated doc when using plt.show
+warnings.filterwarnings("ignore", category=UserWarning,
+    message='Matplotlib is currently using agg, which is a'
+            ' non-GUI backend, so cannot show the figure.')
+
+
+
+# -----------------------------------------------------------------------------
+# Autosummary
+# -----------------------------------------------------------------------------
+
+autosummary_generate = True
+autosummary_generate_overwrite = True
+
+# -----------------------------------------------------------------------------
+# Intersphinx configuration
+# -----------------------------------------------------------------------------
+intersphinx_mapping = {
+    'numpy': ('https://docs.scipy.org/doc/numpy', None),
+    'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
+    'matplotlib': ('http://matplotlib.sourceforge.net/', None),
+}
