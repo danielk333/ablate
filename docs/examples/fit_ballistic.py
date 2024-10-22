da102ca6c765bdfcb3e3f5647c0f49735fa8b20a
danielk <daniel.kastinen@irf.se>
2024-10-22 14:13:56 +0200

    added ballistic fit and allowed for varied start vel

---
+++ docs/examples/fit_ballistic.py
@@ -0,0 +1,82 @@
+"""
+Ballistic model usage
+=======================
+
+"""
+
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+from ablate.physics import ballistic
+import ablate
+
+HERE = Path(__file__).parent.absolute()
+data_file = HERE / "data" / "20140104-025436.json"
+
+# Load some data
+data = {}
+with open(data_file, "r") as fh:
+    json_data = json.load(fh)
+data["vel"] = np.array([x if x is not None else np.nan for x in json_data.pop("velocity")])
+data["alt"] = np.array([x if x is not None else np.nan for x in json_data.pop("height")])
+data["vel_std"] = np.array([x if x is not None else np.nan for x in json_data.pop("vel_std")])
+data["alt_std"] = np.array([x if x is not None else np.nan for x in json_data.pop("alt_std")])
+data["t"] = np.array(json_data["t"])
+meta = {
+    "date": json_data.pop("date"),
+    "slope": json_data.pop("slope"),
+}
+
+not_nans = np.logical_not(
+    np.logical_or.reduce(
+        [
+            np.isnan(data["alt"]),
+            np.isnan(data["vel"]),
+        ]
+    )
+)
+data["alt"] = data["alt"][not_nans]
+data["vel"] = data["vel"][not_nans]
+data["vel_std"] = data["vel_std"][not_nans]
+
+
+atm = ablate.atmosphere.AtmPymsis()
+msis_density = atm.density(
+    time=np.datetime64("2018-06-28T12:45:33"),
+    lat=69.5866115,
+    lon=19.221555,
+    alt=data["alt"],
+)["Total"].values.squeeze()
+
+cB_est, v0 = ballistic.fit_velocity(
+    data["alt"],
+    data["vel"],
+    msis_density,
+    weights=data["vel_std"],
+    radiant_local_elevation=meta["slope"],
+    degrees=True,
+)
+inds = np.argsort(data["alt"])[::-1]
+fit_vels = ballistic.velocity(
+    data["alt"][inds],
+    msis_density[inds],
+    cB_est,
+    radiant_local_elevation=meta["slope"],
+    initial_velocity=v0,
+    degrees=True,
+)
+
+print(f"{cB_est=} {v0*1e-3} km/s")
+
+fig, ax = plt.subplots()
+
+ax.set_title("Ballistic coefficient fit")
+ax.plot(data["vel"] * 1e-3, data["alt"] * 1e-3, ".b", label="Data")
+ax.plot(fit_vels * 1e-3, data["alt"][inds] * 1e-3, "-r", label="Fit")
+ax.set_xlabel("Velocity [km/s]")
+ax.set_ylabel("Height [km]")
+ax.legend()
+
+plt.show()
--- src/ablate/physics/ballistic.py
+++ src/ablate/physics/ballistic.py
@@ -11,29 +11,50 @@ def velocity(
     initial_velocity=None,
     degrees=False,
 ):
+    """
+
+    Notes
+    -----
+    Assumes atmospheric mass densities are sorted according to time
+    """
     el = np.radians(radiant_local_elevation) if degrees else radiant_local_elevation
     alpha = 0.5 / (ballistic_coefficient * np.sin(el))
 
     int_atm_density = sci.cumulative_trapezoid(atm_mass_density, heights)
     v = np.exp(alpha * int_atm_density)
+    v = np.insert(v, 0, 1.0)
 
     if initial_velocity is not None:
         v *= initial_velocity
     return v
 
 
-def _LSQ_fit_func(cB, heights, velocities, weights, atm_dens, el, degrees):
+def _LSQ_fit_func(cB, heights, velocities, initial_velocity, weights, atm_dens, el, degrees):
     pred_v = velocity(
         heights,
         atm_dens,
         cB,
         el,
-        initial_velocity=velocities[0],
+        initial_velocity=initial_velocity,
         degrees=degrees,
     )
-    vels = velocities[1:] if velocities.size - pred_v.size == 1 else velocities
     w = weights[1:] if weights.size - pred_v.size == 1 else weights
-    lsq = np.sum(((pred_v - vels)/w) ** 2)
+    lsq = np.sum(((pred_v - velocities)/w) ** 2)
+    return lsq
+
+
+def _LSQ_fit_func_two_vars(x, heights, velocities, weights, atm_dens, el, degrees):
+    cB, initial_velocity = x
+    pred_v = velocity(
+        heights,
+        atm_dens,
+        cB,
+        el,
+        initial_velocity=initial_velocity,
+        degrees=degrees,
+    )
+    w = weights[1:] if weights.size - pred_v.size == 1 else weights
+    lsq = np.sum(((pred_v - velocities)/w) ** 2)
     return lsq
 
 
@@ -42,15 +63,27 @@ def fit_velocity(
     velocities,
     atm_mass_density,
     radiant_local_elevation,
+    initial_velocity=None,
+    start=None,
     weights=None,
     degrees=False,
     minimize_kwargs={},
 ):
     if weights is None:
         weights = np.ones_like(velocities)
+    if initial_velocity is None:
+        x0 = (1.0, velocities[0])
+        _obj_func = _LSQ_fit_func_two_vars
+    else:
+        x0 = (1.0, )
+        _obj_func = _LSQ_fit_func
+
+    if start is None:
+        start = x0
+
     result = sco.minimize(
-        _LSQ_fit_func,
-        x0=(1.0,),
+        _obj_func,
+        x0=start,
         args=(
             heights,
             velocities,
@@ -61,4 +94,4 @@ def fit_velocity(
         ),
         **minimize_kwargs
     )
-    return result.x[0]
+    return result.x
