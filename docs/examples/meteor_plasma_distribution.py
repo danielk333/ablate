6391c0d41fd2ef7397913c4194996fd6b8c2945d
Daniel Kastinen <daniel.kastinen@irf.se>
2025-09-24 19:43:06 +0200

    do model running, caching and scaling tbd

--- docs/examples/meteor_plasma_distribution.py
+++ docs/examples/meteor_plasma_distribution.py
@@ -1,12 +1,15 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import ablate.atmosphere as atm
 import ablate.models.dimant_oppenheim_2017 as do
+
 
 met_vel = 40e3
 ablated_thermal_vel = 951
 met_r = 5e-5
-ma=4.7e-26
+ma = 4.7e-26
 n0 = 10**13 / met_r**2
 rel_vel = met_vel - ablated_thermal_vel
 sigma = do.collisional_cross_section_bronshten_1983(rel_vel)
@@ -24,21 +27,69 @@ atm_data = model.density(
 index = dict(lat=0, lon=0, alt=0, time=0)
 na = atm_data["Total"][index].values / ma
 
-ne_dist = do.plasma_distribution(
-    total_atmospheric_number_density=na,
-    meteoroid_velocity=met_vel,
-    meteoroid_radius=met_r,
-    plasma_source_density=n0,
-    collisional_cross_section=sigma,
-    ionization_probability=beta,
-    ablated_thermal_speed=ablated_thermal_vel,
-    atmospheric_species_mass=ma,
-    ablated_species_mass=3.8e-26,
-    grid_size=(201, 201),
-    grid_step=(3.9e-4, 3.9e-4)
+lam = do.mean_free_path(
+    ablated_thermal_vel,
+    na,
+    sigma,
+    met_vel,
 )
+# TODO: pre-compute the F functions that are independant on meteoroid parameters and create a high
+# resolution version that can be scaled quickly
+if False:
+    x, y, ne_dist = do.plasma_distribution(
+        total_atmospheric_number_density=na,
+        meteoroid_velocity=met_vel,
+        meteoroid_radius=met_r,
+        plasma_source_density=n0,
+        collisional_cross_section=sigma,
+        ionization_probability=beta,
+        ablated_thermal_speed=ablated_thermal_vel,
+        atmospheric_species_mass=ma,
+        ablated_species_mass=3.8e-26,
+        # x_grid=(np.arange(301) - 100)*0.0003,
+        # y_grid=(np.arange(201) - 100)*0.0003,
+        x_grid=(np.arange(401) - 100) * 0.005,
+        y_grid=(np.arange(201) - 100) * 0.005,
+        threads=6,
+    )
+    np.save("/home/danielk/data/ne_dist_test.npy", ne_dist)
+    np.save("/home/danielk/data/x_test.npy", x)
+    np.save("/home/danielk/data/y_test.npy", y)
+ne_dist = np.load("/home/danielk/data/ne_dist_test.npy")
+x = np.load("/home/danielk/data/x_test.npy")
+y = np.load("/home/danielk/data/y_test.npy")
+f_p = do.plasma_frequency(ne_dist)
+
+
+def plasma_freq_to_area(dx, dy, f_p, f_p_limit):
+    area_unit = dx * dy
+    return np.sum(f_p > f_p_limit) * area_unit
+
 
 fig, ax = plt.subplots()
-ax.pcolormesh(np.log10(ne_dist))
+cmap = "jet"
+im = ax.pcolormesh(x / lam, y / lam, ne_dist, cmap=cmap, norm=LogNorm())
+fig.colorbar(im, ax=ax)
+ax.set_xlabel("x [lambda_T]")
+ax.set_ylabel("y [lambda_T]")
+
+
+fig, ax = plt.subplots()
+cmap = "jet"
+im = ax.pcolormesh(x, y, f_p, cmap=cmap, norm=LogNorm())
+cs = ax.contour(x, y, f_p, levels=[50e6, 220e6, 500e6, 930e6, 1300e6], colors="black")
+ax.clabel(cs, cs.levels, fmt="%.2e")
+fig.colorbar(im, ax=ax)
+ax.set_xlabel("x [m]")
+ax.set_ylabel("y [m]")
+
+f = np.linspace(10e6, 1e9, 100)
+area = np.empty_like(f)
+for ind in range(len(f)):
+    area[ind] = plasma_freq_to_area(0.01, 0.01, f_p, f[ind])
+
+fig, ax = plt.subplots()
+ax.loglog(f, area)
+
 
 plt.show()
--- src/ablate/models/dimant_oppenheim_2017.py
+++ src/ablate/models/dimant_oppenheim_2017.py
@@ -1,7 +1,25 @@
+import ctypes
 import numpy as np
 import scipy.special as scs
 import scipy.integrate as sci
+import scipy.constants as consts
 from tqdm import tqdm
+import multiprocessing as mp
+
+
+def plasma_frequency(electron_density):
+    return np.sqrt(electron_density * consts.e**2 / (consts.epsilon_0 * consts.m_e)) / (2 * np.pi)
+
+
+def mean_free_path(
+    ablated_thermal_speed,
+    total_atmospheric_number_density,
+    collisional_cross_section,
+    meteoroid_velocity,
+):
+    return ablated_thermal_speed / (
+        total_atmospheric_number_density * collisional_cross_section * meteoroid_velocity
+    )
 
 
 def collisional_cross_section_bronshten_1983(relative_velocity):
@@ -21,12 +39,15 @@ def plasma_distribution(
     plasma_source_density,
     collisional_cross_section,
     ionization_probability,
-    grid_size=(301, 301),
-    grid_step=(0.05, 0.05),
+    x_grid,
+    y_grid,
     ablated_thermal_speed=951,
     atmospheric_species_mass=4.7e-26,
     ablated_species_mass=3.8e-26,
-    quad_abs_tol=1e-8,
+    quad_abs_tol=1e-7,
+    upper_limit_delta=1e-6,
+    threads=None,
+    pbar=True,
 ):
     """Calculate meteor head plasma distribution using model from Dimant and
     Oppenheim 2017
@@ -36,33 +57,11 @@ def plasma_distribution(
     Paper available at https://doi.org/10.1029/2021JA029525
     Ported to Python by Daniel Kastinen 2025
 
-    Inputs:
-           in: structure containing DO parameters
-                meteoroid_velocity: meteor velocity, m/s
-                altitude: meteor ablation altitude, m
-                rM: meteoroid radius, m
-                n0: surface density, m**-3
-
-    Outputs:
-           q: line density, # e-/m
-
     ablated_thermal_speed = 951  #thermal speed of ablated particles, m/s
-
+    # Set meteoroid and atmospheric characteristics
+    # atmospheric_species_mass = 4.7e-26  #atmospheric species mass (N2), kg
+    # ablated_species_mass = 3.8e-26  #ablated species mass (Na), kg
     """
-
-    assert len(grid_size) == len(grid_step), "grid size and step must equal"
-    if len(grid_size) == 3:
-        Nx, Ny, Nz = grid_size
-        dx, dy, dz = grid_step
-    elif len(grid_size) == 2:
-        Nx, Ny = grid_size
-        dx, dy = grid_step
-    else:
-        raise NotImplementedError("unrecogized grid size")
-
-    # todo: remove singularity at middle?
-    x = -(Nx - 1) / 2 * dx + dx * np.arange(Nx - 1)
-    y = -(Ny - 1) / 2 * dy + dy * np.arange(Ny - 1)
 
     # mean free path
     lamT = ablated_thermal_speed / (
@@ -70,14 +69,8 @@ def plasma_distribution(
     )
 
     # Calculate the ionizing cross section Gion(meteoroid_velocity)
-    Gcoll = collisional_cross_section / (
-        4 * np.pi
-    )  # G(meteoroid_velocity), net differential cross section
-    Gion = Gcoll * ionization_probability  # Gion(meteoroid_velocity)
-
-    # Set meteoroid and atmospheric characteristics
-    # atmospheric_species_mass = 4.7e-26  #atmospheric species mass (N2), kg
-    # ablated_species_mass = 3.8e-26  #ablated species mass (Na), kg
+    Gcoll = collisional_cross_section / (4 * np.pi)  # net differential cross section
+    Gion = Gcoll * ionization_probability
 
     coeff_const = (
         8
@@ -92,86 +85,168 @@ def plasma_distribution(
 
     # Generate density distribution
     # Solve for a 2D plane
-    [X2, Y2] = np.meshgrid(x, y)
+    [X2, Y2] = np.meshgrid(x_grid, y_grid)
 
     R = np.sqrt(X2**2 + Y2**2)
-    theta = np.atan2(Y2, X2)
+    R[R == 0] = np.nan
 
-    # Define some common factors: cos(θ), sin(θ), R/λΤ and (R/λT)**(2/3)
-    costh = np.cos(theta)
-    sinth = np.sin(theta)
+    costh = X2 / R
+    # costh[costh == 0] == np.nan
+    sinth = Y2 / R
     Rlam = R / lamT
     Rlam23 = Rlam ** (2 / 3)
 
     coeff = coeff_const / R
 
     Func1 = np.sqrt(2 * np.pi / 3) / Rlam * scs.erf(
-        np.sqrt(3 / 2) * Rlam ** (1 / 3) * (np.abs(costh)) ** (1 / 3)
-    ) - np.exp(-3 / 2 * Rlam23 * (np.abs(costh)) ** (2 / 3)) * (
+        np.sqrt(3 / 2) * Rlam ** (1 / 3) * np.abs(costh) ** (1 / 3)
+    ) - np.exp(-3 / 2 * Rlam23 * np.abs(costh) ** (2 / 3)) * (
         (4 - np.pi)
         * np.abs(costh)
-        / (2 * np.sqrt((1 + (4 - np.pi) ** 2 * Rlam23 * (np.abs(costh)) ** (2 / 3)) / (2 * np.pi)))
-        + 2 * (np.abs(costh)) ** (1 / 3) / Rlam23
+        / (2 * np.sqrt((1 + (4 - np.pi) ** 2 * Rlam23 * np.abs(costh) ** (2 / 3)) / (2 * np.pi)))
+        + 2 * np.abs(costh) ** (1 / 3) / Rlam23
     )
 
     Func2 = np.sqrt(2 * np.pi / 3) / Rlam * scs.erf(np.sqrt(3 * Rlam23 / 2)) - (
         1 + 2 / Rlam23
     ) * np.exp(-3 * Rlam23 / 2)
 
-    alpha = 1e-6
-
-    # ksi == ξ in Dimant and Oppenheim's notation
-    def _do_integral_1(ksi, rlam, cth):
+    def _do_integral_1(ksi, rlam23, cth):
         return (
-            np.sqrt(1 + 2 * rlam * ksi ** (2 / 3) / np.pi)
-            * np.exp(-3 * rlam * ksi ** (2 / 3) / 2)
+            np.sqrt(1 + 2 * rlam23 * ksi ** (2 / 3) / np.pi)
+            * np.exp(-3 * rlam23 * ksi ** (2 / 3) / 2)
             * np.sqrt((ksi**2 - cth**2) / (1 - ksi**2))
         )
 
-    def _do_integral_2(ksi, rlam, cth, sth):
+    def _do_integral_2(ksi, rlam23, cth, sth):
         return (
-            np.sqrt(1 + 2 * rlam * ksi ** (2 / 3) / np.pi)
-            * np.exp(-3 * rlam * ksi ** (2 / 3) / 2)
+            np.sqrt(1 + 2 * rlam23 * ksi ** (2 / 3) / np.pi)
+            * np.exp(-3 * rlam23 * ksi ** (2 / 3) / 2)
             * np.arcsin(np.sqrt(1 - ksi**2) * np.abs(cth) / (ksi * np.abs(sth)))
         )
 
-    def do_int_1_quad(a, b, rlam, cth):
-        return sci.quad(_do_integral_1, a, b, args=(rlam, cth), epsrel=0, epsabs=quad_abs_tol)[0]
+    def do_int_1_quad(a, b, rlam23, cth):
+        int1 = sci.quad(
+            _do_integral_1,
+            a,
+            b,
+            args=(rlam23, cth),
+            epsrel=0,
+            epsabs=quad_abs_tol,
+        )
+        return int1[0]
 
-    def do_int_2_quad(a, b, rlam, cth, sth):
-        return sci.quad(_do_integral_2, a, b, args=(rlam, cth, sth), epsrel=0, epsabs=quad_abs_tol)[0]
+    def do_int_2_quad(a, b, rlam23, cth, sth):
+        int2 = sci.quad(
+            _do_integral_2,
+            a,
+            b,
+            args=(rlam23, cth, sth),
+            epsrel=0,
+            epsabs=quad_abs_tol,
+        )
+        return int2[0]
 
     orig_shape = costh.shape
-    costh = costh.flatten()
-    sinth = sinth.flatten()
-    Rlam23 = Rlam23.flatten()
+    new_shape = (costh.size,)
+    costh = costh.reshape(new_shape)
+    sinth = sinth.reshape(new_shape)
+    Rlam23 = Rlam23.reshape(new_shape)
     low_lim = np.abs(costh)  # Lower limit of integration |cosθ|
     up_lim = np.full_like(
-        costh, 1 - alpha
+        costh, 1 - upper_limit_delta
     )  # Upper limit of integration 1 (avoid singularity at exactly 1)
 
-    Func3_1 = np.empty_like(costh)
-    Func3_2 = np.empty_like(costh)
-    for ind in tqdm(range(len(costh)), total=len(costh)):
-        Func3_1[ind] = do_int_1_quad(low_lim[ind], up_lim[ind], Rlam23[ind], costh[ind])
-        Func3_2[ind] = do_int_2_quad(low_lim[ind], up_lim[ind], Rlam23[ind], costh[ind], sinth[ind])
+    # todo: hack to make sure integration limits are sane
+    low_lim[low_lim > up_lim] = up_lim[low_lim > up_lim] - upper_limit_delta
 
-    # do_int_1_quad_vec = np.vectorize(do_int_1_quad)
-    # do_int_2_quad_vec = np.vectorize(do_int_2_quad)
-    # Func3_1 = do_int_1_quad_vec(low_lim, up_lim, Rlam23, costh)
+    # TODO: skip problematic points in the calc
+
+    if threads is None:
+        if pbar:
+            tqdm_bar = tqdm(desc="computing electron density", total=len(costh))
+        Func3_1 = np.empty_like(costh)
+        Func3_2 = np.empty_like(costh)
+        for ind in range(len(costh)):
+            Func3_1[ind] = do_int_1_quad(low_lim[ind], up_lim[ind], Rlam23[ind], costh[ind])
+            Func3_2[ind] = do_int_2_quad(
+                low_lim[ind], up_lim[ind], Rlam23[ind], costh[ind], sinth[ind]
+            )
+            if pbar:
+                tqdm_bar.update(1)
+        if pbar:
+            tqdm_bar.close()
+    else:
+        th = []
+        chunks = np.array_split(np.arange(len(costh)), threads)
+
+        def worker(inds, tind, Func3_1, Func3_2, low_lim, up_lim, Rlam23, costh, sinth, pbar):
+            if pbar:
+                tqdm_bar = tqdm(
+                    desc=f"computing electron density process {tind}",
+                    total=len(inds),
+                    position=tind,
+                )
+            f31 = np.empty(inds.shape, dtype=costh.dtype)
+            f32 = np.empty(inds.shape, dtype=costh.dtype)
+            for lind, ind in enumerate(inds):
+                f31[lind] = do_int_1_quad(low_lim[ind], up_lim[ind], Rlam23[ind], costh[ind])
+                f32[lind] = do_int_2_quad(
+                    low_lim[ind], up_lim[ind], Rlam23[ind], costh[ind], sinth[ind]
+                )
+                if pbar:
+                    tqdm_bar.update(1)
+            with Func3_1.get_lock():
+                shared_f31 = np.ndarray(costh.shape, dtype=np.float64, buffer=Func3_1.get_obj())
+                shared_f31[inds] = f31
+            with Func3_2.get_lock():
+                shared_f32 = np.ndarray(costh.shape, dtype=np.float64, buffer=Func3_2.get_obj())
+                shared_f32[inds] = f32
+            if pbar:
+                tqdm_bar.close()
+
+        f31 = mp.Array(ctypes.c_double, costh.size)
+        f32 = mp.Array(ctypes.c_double, costh.size)
+        for tind, chunk in enumerate(chunks):
+            t = mp.Process(
+                target=worker,
+                args=(
+                    chunk,
+                    tind,
+                    f31,
+                    f32,
+                    low_lim,
+                    up_lim,
+                    Rlam23,
+                    costh,
+                    sinth,
+                    pbar,
+                ),
+            )
+            th.append(t)
+            t.start()
+
+        for t in th:
+            t.join()
+        print("\n" * threads)
+        Func3_1 = np.ndarray(costh.shape, dtype=np.float64, buffer=f31.get_obj())
+        Func3_2 = np.ndarray(costh.shape, dtype=np.float64, buffer=f32.get_obj())
     Func3_1 = Func3_1.reshape(orig_shape)
-
-    # Func3_2 = do_int_2_quad_vec(low_lim, up_lim, Rlam23, costh, sinth)
     Func3_2 = Func3_2.reshape(orig_shape)
 
     costh = costh.reshape(orig_shape)
     sinth = sinth.reshape(orig_shape)
 
-    t1 = coeff * np.abs(costh) * Func1
-    t2 = coeff * costh * Func2
-    t3 = coeff * (np.abs(costh) * Func3_2 + Func3_1)
+    t1 = np.abs(costh) * Func1
+    t2 = costh * Func2
+    t3 = np.abs(costh) * Func3_2 + Func3_1
     t3[np.isnan(t3)] = 0
-    ne2D = np.abs(t1 + t2 + t3)
-    # ne2D[:, (Ny + 1) / 2] = (ne2D[:, (Ny - 1) / 2 - 1] + ne2D[:, (Ny + 3) / 2 - 1]) / 2
+    ne2D = coeff * np.abs(t1 + t2 + t3)
+    # breakpoint()
 
-    return ne2D
+    # some magic to prevent numerical artifact? todo figure out why
+    # zero_point = np.argmin(np.abs(y_grid))
+    # if y_grid[zero_point] < 1e-3:
+    #     ne2D[:, zero_point] = (ne2D[:, zero_point - 1] + ne2D[:, zero_point + 1]) / 2
+
+    return X2, Y2, ne2D
