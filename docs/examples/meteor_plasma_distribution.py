8b12524dd0ccd9643646e98578a4130b9d7f5e32
Daniel Kastinen <daniel.kastinen@irf.se>
2025-10-31 11:13:01 +0100

    updated example - there is some strange singularity in the model? todo fix that

--- docs/examples/meteor_plasma_distribution.py
+++ docs/examples/meteor_plasma_distribution.py
@@ -1,19 +1,38 @@
 import argparse
+import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import ablate.atmosphere as atm
 import ablate.models.dimant_oppenheim_2017 as do
+import scipy.constants as consts
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--cache", default=None)
+parser.add_argument(
+    "--scale",
+    choices=[0.0005, 0.005, 0.05, 0.5],
+    type=float,
+    default=0.05,
+)
+args = parser.parse_args()
 
 met_vel = 40e3
-ablated_thermal_vel = 951
+met_mu = 3.8e-26
+ablated_thermal_vel = do.ablated_thermal_speed_bronshten_1983(
+    meteoroid_surface_temperature=2000, meteoroid_molecular_mass=met_mu
+)
 met_r = 5e-5
 ma = 4.7e-26
 n0 = 10**13 / met_r**2
 rel_vel = met_vel - ablated_thermal_vel
 sigma = do.collisional_cross_section_bronshten_1983(rel_vel)
 beta = do.ionization_probability_Na_vondrak_2008(rel_vel)
+radar_freqs = np.array([50e6, 220e6, 500e6, 930e6, 1300e6])
+radar_critical_dens = do.critical_plasma_density(radar_freqs)
+
+for f, ne in zip(radar_freqs, radar_critical_dens):
+    print(f"{f=:.2e} -> {ne=:.2e}")
 
 model = atm.AtmPymsis()
 
@@ -33,31 +52,44 @@ lam = do.mean_free_path(
     sigma,
     met_vel,
 )
-# TODO: pre-compute the F functions that are independant on meteoroid parameters and create a high
-# resolution version that can be scaled quickly
-if False:
-    x, y, ne_dist = do.plasma_distribution(
-        total_atmospheric_number_density=na,
-        meteoroid_velocity=met_vel,
-        meteoroid_radius=met_r,
-        plasma_source_density=n0,
-        collisional_cross_section=sigma,
-        ionization_probability=beta,
-        ablated_thermal_speed=ablated_thermal_vel,
-        atmospheric_species_mass=ma,
-        ablated_species_mass=3.8e-26,
-        # x_grid=(np.arange(301) - 100)*0.0003,
-        # y_grid=(np.arange(201) - 100)*0.0003,
-        x_grid=(np.arange(401) - 100) * 0.005,
-        y_grid=(np.arange(201) - 100) * 0.005,
-        threads=6,
+
+if args.cache is not None:
+    cache = pathlib.Path(args.cache)
+    if cache.exists():
+        _dat = np.load(cache)
+        xlam = _dat["x"]
+        ylam = _dat["y"]
+        rlam = _dat["r"]
+        do_f_dist = _dat["f"]
+    else:
+        xlam, ylam, rlam, do_f_dist = do.plasma_distribution_morphology(
+            x_grid=(np.arange(201) - 60) * args.scale,
+            y_grid=(np.arange(201) - 100) * args.scale,
+            threads=8,
+        )
+        np.savez(cache, x=xlam, y=ylam, r=rlam, f=do_f_dist)
+else:
+    xlam, ylam, rlam, do_f_dist = do.plasma_distribution_morphology(
+        x_grid=(np.arange(201) - 60) * args.scale,
+        y_grid=(np.arange(201) - 100) * args.scale,
+        threads=8,
     )
-    np.save("/home/danielk/data/ne_dist_test.npy", ne_dist)
-    np.save("/home/danielk/data/x_test.npy", x)
-    np.save("/home/danielk/data/y_test.npy", y)
-ne_dist = np.load("/home/danielk/data/ne_dist_test.npy")
-x = np.load("/home/danielk/data/x_test.npy")
-y = np.load("/home/danielk/data/y_test.npy")
+
+x = xlam * lam
+y = ylam * lam
+ne_dist = do.plasma_distribution(
+    total_atmospheric_number_density=na,
+    meteoroid_velocity=met_vel,
+    meteoroid_radius=met_r,
+    plasma_source_density=n0,
+    collisional_cross_section=sigma,
+    ionization_probability=beta,
+    ablated_thermal_speed=ablated_thermal_vel,
+    atmospheric_species_mass=ma,
+    ablated_species_mass=met_mu,
+    base_plasma_distribution_function=do_f_dist,
+    R_lambda=rlam,
+)
 f_p = do.plasma_frequency(ne_dist)
 
 
@@ -68,7 +100,7 @@ def plasma_freq_to_area(dx, dy, f_p, f_p_limit):
 
 fig, ax = plt.subplots()
 cmap = "jet"
-im = ax.pcolormesh(x / lam, y / lam, ne_dist, cmap=cmap, norm=LogNorm())
+im = ax.pcolormesh(xlam, ylam, ne_dist, cmap=cmap, norm=LogNorm())
 fig.colorbar(im, ax=ax)
 ax.set_xlabel("x [lambda_T]")
 ax.set_ylabel("y [lambda_T]")
@@ -77,7 +109,7 @@ ax.set_ylabel("y [lambda_T]")
 fig, ax = plt.subplots()
 cmap = "jet"
 im = ax.pcolormesh(x, y, f_p, cmap=cmap, norm=LogNorm())
-cs = ax.contour(x, y, f_p, levels=[50e6, 220e6, 500e6, 930e6, 1300e6], colors="black")
+cs = ax.contour(x, y, f_p, levels=radar_freqs, colors="black")
 ax.clabel(cs, cs.levels, fmt="%.2e")
 fig.colorbar(im, ax=ax)
 ax.set_xlabel("x [m]")
@@ -86,10 +118,12 @@ ax.set_ylabel("y [m]")
 f = np.linspace(10e6, 1e9, 100)
 area = np.empty_like(f)
 for ind in range(len(f)):
-    area[ind] = plasma_freq_to_area(0.01, 0.01, f_p, f[ind])
+    area[ind] = plasma_freq_to_area(args.scale, args.scale, f_p, f[ind])
 
 fig, ax = plt.subplots()
 ax.loglog(f, area)
+ax.set_xlabel("Plasma frequency Fp [Hz]")
+ax.set_ylabel("xy-area with > Fp frequency [m^2]")
 
 
 plt.show()
