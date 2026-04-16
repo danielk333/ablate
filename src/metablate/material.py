242c817f59744e8246466ff182fa5a04aaf96040
Daniel Kastinen <daniel.kastinen@irf.se>
2026-04-16 12:01:54 +0200

    fixed names in material

--- src/metablate/material.py
+++ src/metablate/material.py
@@ -1,18 +1,5 @@
-'''
+"""
 # Meteoroid material physics
-
-List of properties:
- - `m2`: mean atomic mass [1]
- - `u0`: surface binding energy [eV]
- - `k`: average atomic number [1]
- - `CA`, CB: Clausius-Clapeyron coeff [K]
- - `mu`: mean molecular mass of ablated vapour [kg]
- - `k`: Botzmann constant [J/K]
- - `rho_m`: meteoroid density [kg/m3]
- - `c`: specific heat of meteoroid [J/(K*kg)]
- - `L`: latent heat of fusion + vapourization [J/kg]
-
-# TODO: list references for material data
 
 [Fe SiO2 C C] mean molecular mass per target atoms corresponding
 to the different meteoroid densities, see Rogers p. 1344 values
@@ -22,7 +9,8 @@ Mean molecular mass of ablated vapor assuming that all products
 liberated from the meteoroid by thermal ablation were released
 in complete molecular form.
 
-'''
+"""
+
 from dataclasses import dataclass, asdict
 import logging
 from copy import copy
@@ -33,102 +21,86 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class Material:
-    """Container for material properties
+    """Container for material properties"""
 
-    TODO: proper names for all vars
-
-List of properties:
- - `m2`: mean atomic mass [1]
- - `u0`: surface binding energy [eV]
- - `k`: average atomic number [1]
- - `CA`, CB: Clausius-Clapeyron coeff [K]
- - `mu`: mean molecular mass of ablated vapour [kg]
- - `k`: Botzmann constant [J/K]
- - `rho_m`: meteoroid density [kg/m3]
- - `c`: specific heat of meteoroid [J/(K*kg)]
- - `L`: latent heat of fusion + vapourization [J/kg]
-    """
-    rho_m: float
-    mu: float
-    m2: float
-    CA: float
-    CB: float
-    u0: float
-    k: float
-    z2: float
-    c: float
-    L: float
+    bulk_density: float  # [kg/m3]
+    mean_atomic_mass: float  # [kg]
+    latent_heat_of_fusion_vapourization: float  # [J/kg]
+    specific_heat: float  # [J/(K*kg)]
+    average_atomic_number: float  # [1]
+    surface_binding_energy: float  # [eV]
+    ablated_vapour_mean_molecular_mass: float  # [kg]
+    penetration_correction_parameter_K: float  # [1]
+    clausius_clapeyron_coeff_A: float  # [K]
+    clausius_clapeyron_coeff_B: float  # [K]
 
 
 MATERIALS = {}
 
 iron = Material(
-    rho_m = 7800.0,
-    mu = 56.0*constants.u,
-    m2 = 56.0*constants.u,
-    CA = 10.6,
-    CB = 16120.,
-    u0 = 4.1,
-    k = 0.35,
-    z2 = 26,
-    c = 1200,
-    L = 6.0E6,
+    bulk_density=7800.0,
+    mean_atomic_mass=56.0 * constants.u,
+    latent_heat_of_fusion_vapourization=6.0e6,
+    specific_heat=1200,
+    average_atomic_number=26,
+    surface_binding_energy=4.1,
+    ablated_vapour_mean_molecular_mass=56.0 * constants.u,
+    penetration_correction_parameter_K=0.35,
+    clausius_clapeyron_coeff_A=10.6,
+    clausius_clapeyron_coeff_B=16120.0,
 )
 MATERIALS["iron"] = iron
 
 asteroidal = Material(
-    rho_m = 3300.0,
-    mu = 50.0*constants.u,
-    m2 = 20.0*constants.u,
-    CA = 10.6,
-    CB = 13500.,
-    u0 = 6.4,
-    k = 0.1,
-    z2 = 10,
-    c = 1200,
-    L = 6.0E6,
+    bulk_density=3300.0,
+    mean_atomic_mass=20.0 * constants.u,
+    latent_heat_of_fusion_vapourization=6.0e6,
+    specific_heat=1200,
+    average_atomic_number=10,
+    surface_binding_energy=6.4,
+    ablated_vapour_mean_molecular_mass=50.0 * constants.u,
+    penetration_correction_parameter_K=0.1,
+    clausius_clapeyron_coeff_A=10.6,
+    clausius_clapeyron_coeff_B=13500.0,
 )
 MATERIALS["asteroidal"] = asteroidal
 
 cometary = Material(
-    rho_m = 1000.0,
-    mu = 20.0*constants.u,
-    m2 = 12.0*constants.u,
-    CA = 10.6,
-    CB = 13500.,
-    u0 = 4.0,
-    k = 0.65,
-    z2 = 6,
-    c = 1200,
-    L = 6.0E6,
+    bulk_density=1000.0,
+    mean_atomic_mass=12.0 * constants.u,
+    latent_heat_of_fusion_vapourization=6.0e6,
+    specific_heat=1200,
+    average_atomic_number=6,
+    surface_binding_energy=4.0,
+    ablated_vapour_mean_molecular_mass=20.0 * constants.u,
+    penetration_correction_parameter_K=0.65,
+    clausius_clapeyron_coeff_A=10.6,
+    clausius_clapeyron_coeff_B=13500.0,
 )
 MATERIALS["cometary"] = cometary
 
 porous = Material(
-    rho_m = 300.0,
-    mu = 20.0*constants.u,
-    m2 = 12.0*constants.u,
-    CA = 10.6,
-    CB = 13500.,
-    u0 = 4.0,
-    k = 0.65,
-    z2 = 6,
-    c = 1200,
-    L = 6.0E6,
+    bulk_density=300.0,
+    mean_atomic_mass=12.0 * constants.u,
+    latent_heat_of_fusion_vapourization=6.0e6,
+    specific_heat=1200,
+    average_atomic_number=6,
+    surface_binding_energy=4.0,
+    ablated_vapour_mean_molecular_mass=20.0 * constants.u,
+    penetration_correction_parameter_K=0.65,
+    clausius_clapeyron_coeff_A=10.6,
+    clausius_clapeyron_coeff_B=13500.0,
 )
 MATERIALS["porous"] = porous
 
 
 def available():
-    '''List currently available meteoroid materials.
-    '''
+    """List currently available meteoroid materials."""
     return list(MATERIALS.keys())
 
 
 def get(material, as_dict=True):
-    '''Returns the physical parameters of the meteoroid based on its material.
-
-    # TODO: figure out if we keep as dict or keep as material???
+    """Returns the physical parameters of the meteoroid based on its material.
 
     Parameters
     ----------
@@ -141,13 +113,12 @@ def get(material, as_dict=True):
     -------
     Material or dict
         Dictionary of all available material parameters (float or None)
-    '''
+    """
 
     _material = material.lower().strip()
     if _material not in MATERIALS:
         raise ValueError(
-            f'No data exists for material "{_material}"\n'
-            f'Avalible materials are: {available()}'
+            f'No data exists for material "{_material}"\n' f"Avalible materials are: {available()}"
         )
     if as_dict:
         return asdict(MATERIALS[_material])
