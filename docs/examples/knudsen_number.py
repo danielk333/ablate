60918fae12c24f8f58ff309b85d1c004d5572b41
Daniel Kastinen <daniel.kastinen@irf.se>
2026-05-24 11:23:23 +0200

    fixed knutsen example

--- docs/examples/knudsen_number.py
+++ docs/examples/knudsen_number.py
@@ -11,33 +11,38 @@ import matplotlib.pyplot as plt
 import metablate.physics as phys
 import metablate
 
-# data for 2 different meteoroids
+alts = np.array([100e3, 80e3, 60e3])
+
 model = metablate.atmosphere.AtmPymsis()
 data = model.density(
     time=np.datetime64("2018-07-28"),
     lat=np.array([69.0]),
     lon=np.array([12.0]),
-    alt=np.array([80e3]),
+    alt=alts,
 )
-atm_total_mass_density = data["Total"].values.squeeze()
-atm_total_number_density = atm_total_mass_density / model.mean_mass
+atm_total_mass_density = data["Total"].values.flatten()
+atm_total_number_density = np.zeros_like(atm_total_mass_density)
+for symbol in model.species:
+    s_dens = data[symbol].values.flatten()
+    inds = np.logical_not(np.isnan(s_dens))
+    atm_total_number_density[inds] = atm_total_number_density[inds] + s_dens[inds]
 
-print(data)
+material_data = metablate.material.asteroidal
 
-material_data = metablate.material.get("asteroidal")
-
-log_masses = np.linspace(-9, -2, 200)
+log_masses = np.linspace(-9, 4, 200)
 masses = 10.0**log_masses
 
-Kn_inf, L = phys.thermal_ablation.Knudsen_number_kero_szasz_2008(
-    masses, material_data["rho_m"], atm_total_number_density
-)
+fig, ax = plt.subplots(figsize=(7, 4), layout="tight")
 
-fig, ax = plt.subplots(figsize=(7, 4))
-
-ax.loglog(masses, Kn_inf, c="k")
-ax.axhline(0.01, c="r", label="Continuum flow")
-ax.axhline(10.0, c="g", label="Free molecular flow")
+for ind in range(len(alts)):
+    Kn_inf, L = phys.thermal_ablation.Knudsen_number_kero_szasz_2008(
+        masses,
+        material_data.bulk_density,
+        atm_total_number_density[ind],
+    )
+    ax.loglog(masses, Kn_inf, label=f"Meteoroid at {alts[ind] * 1e-3:.0f} km altitude")
+ax.axhline(0.01, c="r", ls="--", label="Continuum flow")
+ax.axhline(10.0, c="g", ls="--", label="Free molecular flow")
 ax.set_xlabel("Mass [kg]")
 ax.set_ylabel("Knudsen number")
 ax.legend()
